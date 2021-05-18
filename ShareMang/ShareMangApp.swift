55d9d2f8bddf1e926324b6af4f28aa2b5b25490d
Thaksina Jansaengsri <32285839+Thaksina@users.noreply.github.com>
2021-05-18 12:19:39 +0700

    Initial Commit

---
+++ ShareMang/ShareMangApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareMangApp.swift
+//  ShareMang
+//
+//  Created by Thaksina on 18/5/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct ShareMangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
